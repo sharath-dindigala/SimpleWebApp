17b53bfc687ca765f0068bd0b914eedf1c38994e
Casey Kriutzfield <casey.kriutzfield@microsoft.com>
2022-12-05 13:57:31 -0500

    Initial

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimpleWebApp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    public readonly string MachineName;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+        MachineName = Environment.MachineName;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SimpleWebApp
+
+This is a __really__ simple web application that is used to demo containers in .NET core.
+
+Currently using .NET core 7.
